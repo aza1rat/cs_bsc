54a70264f03eb480c059e30915de7b276fb59e2d
ArtyomAzartny <azazartnykash@gmail.com>
2021-12-10 16:18:07 +0300

    07.12 release

---
+++ azarat_cs_classmas/BankSchetCs/BankSchetCs/BankSchetClass.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSchetCs
+{
+    class BankSchetClass
+    {
+        private uint number;
+        private DateTime dateOpen;
+        private Fio fio;
+        private double balance;
+        private int month;
+
+        public uint Number { get { return number; } }
+        public DateTime DateOpen { get { return dateOpen; } set { dateOpen = value; } }
+        public Fio FIO { get { return fio; } }
+        public double Balance { get { return balance; } set { balance = value; } }
+        public int Month { get { return month; } set { month = value; } }
+
+        public BankSchetClass(uint num, DateTime date, Fio fioo, double blnc, int mon)
+        {
+            number = num;
+            dateOpen = date;
+            fio = fioo;
+            balance = blnc;
+            month = mon;
+        }
+
+        public void ImportSchet(double money)
+        {
+            balance += money;
+        }
+
+        public void ExportSchet (double money)
+        {
+            if (money < balance)
+                balance -= money;
+        }
+
+        public void ExportAll ()
+        {
+            if (balance > 0)
+                balance = 0;
+        }
+
+        public void Info ()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Номер счета: {number} " +
+                $"\nФИО: {fio.sName} {fio.fName} " +
+                $"{fio.tName}\nДата открытия:{dateOpen.Date} " +
+                $"\nСрок вклада: {month}" +
+                $"\nДата закрытия: {InfoClose()}" +
+                $"\nБаланс: {balance.ToString("F2")}\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static BankSchetClass operator +(BankSchetClass bsc1, BankSchetClass bsc2)
+        {
+            if (bsc1 != bsc2)
+            {
+                bsc1.Balance = bsc1.Balance + bsc2.Balance;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Операция отклонена. Невозможно объеденить счёт с тем же счетом");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            return bsc1;
+        }
+
+        public static BankSchetClass operator%(BankSchetClass bsc, double proc)
+        {
+            if (proc <= 30)
+                bsc.balance *= Math.Pow((1+proc / 100 /365),bsc.month*30);
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Операция отклонена. На больше, чем 30% годовых мы не согласны");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+           
+            return bsc;
+        }
+
+        public static BankSchetClass operator++(BankSchetClass bsc)
+        {
+            bsc.balance *= Math.Pow((1.0 + 4.7 / 100.0 / 365.0), 30);
+
+            return bsc;
+        }
+
+        public static bool operator==(BankSchetClass bsc1, BankSchetClass bsc2)
+        {
+            if (bsc1.Balance == bsc2.Balance)
+                return true;
+
+            return false;
+        }
+
+        public static bool operator!=(BankSchetClass bsc1, BankSchetClass bsc2)
+        {
+            if (bsc1.Balance == bsc2.Balance)
+                return false;
+                return true;
+        }
+
+        public static BankSchetClass operator-(BankSchetClass bsc, double money)
+        {
+            if (money <= bsc.balance)
+                bsc.balance -= money;
+            return bsc;
+        }
+
+        public static BankSchetClass operator-(BankSchetClass bsc1, BankSchetClass bsc2)
+        {
+            double money = bsc1.Balance ;
+            bsc1.balance -= money;
+            bsc2.balance += money;
+            return bsc2;
+        }
+
+        public static BankSchetClass operator+(BankSchetClass bsc, double money)
+        {
+            bsc.balance+= money;
+            return bsc;
+        }
+
+
+        public void Transfer (BankSchetClass bankSchet, double money)
+        {
+            if (money <= balance)
+            {
+                bankSchet.balance += money;
+                balance -= money;
+            }
+        }
+
+        private DateTime InfoClose ()
+        {
+            DateTime data = dateOpen.AddMonths(month);
+            return data.Date;
+            
+        }
+
+        private static uint GenerateNumber()
+        {
+            Random ran = new Random();
+            uint key; string genKey = "";
+            for (int j = 0; j < 3; j++)
+            {
+                    genKey += ran.Next(0, 10).ToString();
+            }
+            key = Convert.ToUInt32(genKey);
+               
+            return key;
+        }
+    }
+}
---
+++ azarat_cs_classmas/BankSchetCs/BankSchetCs/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSchetCs
+{
+
+    public struct Fio
+    {
+        public String sName;
+        public String fName;
+        public String tName;
+    }
+
+    class Program
+    {
+        public static BankSchetClass[] BankSchets = new BankSchetClass[2];
+        public static Random rand = new Random();
+        public static Fio FIO;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < BankSchets.Length; i++)
+            {
+                OpenSchet(ref BankSchets[i]);
+            }
+            while (true)
+            {
+                uint selectSchet = ControlUInt("Введите номер счёта " + GetNumbers(BankSchets));
+                string choice = "";
+                for (int i = 0; i < BankSchets.Length; i++)
+                {
+                    if (BankSchets[i].Number == selectSchet)
+                    {
+                       while(choice != "0")
+                        {
+                            choice = WriteMessage("Выберите опцию:\n1) Вывести информацию о счёте\n2) Вывести деньги со счёта\n3) Вывести все деньги" +
+                                "\n4) Пополнить счёт \n5) Перевод\nПерегрузка операция\n\n6) Объединение двух счетов\n7) Начисление процентов в конце всего периода" +
+                                "\n8) Начисление процентов в конце месяце\n9) Сравнение счетов\n10) Снятие средств с счета\n11) Пополнение средств\n12) Перевод всех средств");
+                            switch (choice)
+                            {
+                                case "1": BankSchets[i].Info();break;
+                                case "2": BankSchets[i].ExportSchet(ControlBalance("Введите сумму для вывода")); break;
+                                case "3": BankSchets[i].ExportAll();break;
+                                case "4": BankSchets[i].ImportSchet(ControlBalance("Введите сумму для вывода")); break;
+                                case "5": TransferSchet(ref BankSchets, BankSchets[i], choice);break;
+                                case "6": TransferSchet(ref BankSchets, BankSchets[i], choice); break;
+                                case "7": BankSchets[i] = BankSchets[i] % ControlBalance("Введите кол-во процентов (не больше 30)");break;
+                                case "8": BankSchets[i]++;break;
+                                case "9": TransferSchet(ref BankSchets, BankSchets[i], choice); break;
+                                case "10": BankSchets[i] = BankSchets[i] - ControlBalance("Введите кол-во средств для снятия"); break;
+                                case "11": BankSchets[i] = BankSchets[i] + ControlBalance("Введите кол-во средств для пополнения"); break;
+                                case "12": TransferSchet(ref BankSchets, BankSchets[i], choice); break;
+                                case "13": Array.Resize(ref BankSchets, BankSchets.Length + 1); OpenSchet(ref BankSchets[BankSchets.Length - 1]);break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        static void OpenSchet(ref BankSchetClass bankSchets)
+        {
+            FIO.sName = WriteMessage("Введите Фамилию");
+            FIO.fName = WriteMessage("Введите имя");
+            FIO.tName = WriteMessage("Введите отчество");
+
+            bankSchets = new BankSchetClass(GenerateNumber(), GenerateDate(), FIO, ControlBalance("Введите баланс"), ControlInt("Введите кол-во месяцев"));
+        }
+
+
+        static string WriteMessage(string mes)
+        {
+            
+            Console.WriteLine(mes);
+            string result = Console.ReadLine();
+            return result;
+        }
+
+        static void TransferSchet(ref BankSchetClass[] bankSchets, BankSchetClass schet, string choice)
+        {
+            uint selectSchet = ControlUInt("Введите номер счёта");
+
+            for (int i = 0; i < bankSchets.Length; i++)
+            {
+                if (selectSchet == bankSchets[i].Number)
+                    switch (choice)
+                    {
+                        case "5": schet.Transfer(bankSchets[i], ControlBalance("Введите сумму для вывода"));break;
+                        case "6": schet = bankSchets[i] + schet; break;
+                        case "9": if (bankSchets[i] == schet)
+                                Console.WriteLine("Баланс выбранных счетов одинаков");
+                            if (bankSchets[i] != schet)
+                                Console.WriteLine("Баланс выбранных счетов разные");break;
+                        case "12": bankSchets[i] = schet - bankSchets[i]; break;
+
+                    }
+            }
+        }
+
+        static uint ControlUInt(string mess)
+        {
+            uint rezult; string temp;
+            do
+            {
+                Console.WriteLine(mess);
+                temp = Console.ReadLine();
+            }
+            while (!uint.TryParse(temp, out rezult));
+            return (rezult);
+        }
+
+        static int ControlInt(string mess)
+        {
+            int rezult; string temp;
+            do
+            {
+                Console.WriteLine(mess);
+                temp = Console.ReadLine();
+            }
+            while (!int.TryParse(temp, out rezult));
+            return (rezult);
+        }
+
+        static double ControlBalance(string mess)
+        {
+            double rezult; string temp;
+            do
+            {
+                do
+                {
+                    Console.WriteLine(mess);
+                    temp = Console.ReadLine();
+                }
+                while (!double.TryParse(temp, out rezult));
+            }
+            while (rezult < 0);
+            rezult = Math.Round(rezult, 2);
+            return (rezult);
+        }
+
+        static DateTime GenerateDate()
+        {
+            int minday, maxday, days;
+            if (DateTime.Now.Day > 5)
+            {
+                minday = DateTime.Now.Day - 5;
+                maxday = DateTime.Now.Day;
+            }
+            else
+            {
+                minday = DateTime.Now.Day;
+                maxday = DateTime.Now.Day + 5;
+            }
+            days = rand.Next(minday, maxday);
+            DateTime dat = new DateTime(DateTime.Now.Year, DateTime.Now.Month, days);
+            return dat;
+
+        }
+
+        static string GetNumbers(BankSchetClass[] bankSchets)
+        {
+            string result = "";
+            for (int i = 0; i < bankSchets.Length; i++)
+            {
+                result += bankSchets[i].Number.ToString() + " ";
+            }
+            return result;
+        }
+
+        private static uint GenerateNumber()
+        {
+            Random ran = new Random();
+            uint key; string genKey = "";
+            for (int j = 0; j < 3; j++)
+            {
+                genKey += ran.Next(0, 10).ToString();
+            }
+            key = Convert.ToUInt32(genKey);
+
+            return key;
+        }
+    }
+}
